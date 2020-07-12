bd148f47e843ac64754a30a415ef02eb0462557f
Dipesh Koirala <dipeshkoirala21@gmail.com>
2020-07-12 09:17:19 +0545

    blog app

---
+++ src/build/precache-manifest.ffb0d8fae1a5604291891ecd42fc4282.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14895dcddd07a236e3fff09ec88d41ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6da7a4e4e50b69e76b69",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "d6b4df46a300acf5ade2",
+    "url": "/static/js/2.66ef6253.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.66ef6253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6da7a4e4e50b69e76b69",
+    "url": "/static/js/main.b96dfc5f.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
